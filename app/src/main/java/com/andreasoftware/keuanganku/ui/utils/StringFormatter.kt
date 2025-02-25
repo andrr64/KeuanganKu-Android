f8f63034b36981df1a816446ad4bad1c48d2aa45
andrr64 <derzandrr03@gmail.com>
2025-02-25 18:58:47 +0700

    add strinf formatter utility

---
+++ app/src/main/java/com/andreasoftware/keuanganku/ui/utils/StringFormatter.kt
@@ -0,0 +1,13 @@
+package com.andreasoftware.keuanganku.ui.utils
+
+import java.text.NumberFormat
+import java.util.Locale
+
+class StringFormatter {
+    companion object {
+        fun formatNumber(value: Double, locale: Locale = Locale("id", "ID")): String {
+            val formatter = NumberFormat.getCurrencyInstance(locale)
+            return formatter.format(value)
+        }
+    }
+}
