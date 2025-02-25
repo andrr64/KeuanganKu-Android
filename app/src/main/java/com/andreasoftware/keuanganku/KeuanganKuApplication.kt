b30f8e2d130735413c801f5073ad25b2d7fcfc63
andrr64 <derzandrr03@gmail.com>
2025-02-26 04:23:48 +0700

    menambahkan kelas KeuanganKuApplication

---
+++ app/src/main/java/com/andreasoftware/keuanganku/KeuanganKuApplication.kt
@@ -0,0 +1,9 @@
+package com.andreasoftware.keuanganku
+
+import android.app.Application
+
+class KeuanganKuApplication: Application() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+}
