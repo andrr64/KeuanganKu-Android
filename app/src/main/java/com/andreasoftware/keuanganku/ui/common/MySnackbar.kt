e3b3488aa3da001f13c6a443d8dba413c27e7fa1
andrr64 <derzandrr03@gmail.com>
2025-03-10 05:07:49 +0700

    Refactor Snackbar background color to use ContextCompat
    The Snackbar background color has been refactored to use `ContextCompat.getColor()` for better resource handling.
    - `MySnackbar`: Changed the background color of the success Snackbar from `android.graphics.Color.GREEN` to `ContextCompat.getColor(view.context, R.color.green)`
    - `HandleExceptionWithSnackbar`: Changed the background color of the failed Snackbar from `android.graphics.Color.RED` to `ContextCompat.getColor(view.context, R.color.red)`

--- app/src/main/java/com/andreasoftware/keuanganku/ui/common/MySnackbar.kt
+++ app/src/main/java/com/andreasoftware/keuanganku/ui/common/MySnackbar.kt
@@ -1,6 +1,8 @@
 package com.andreasoftware.keuanganku.ui.common
 
 import android.view.View
+import androidx.core.content.ContextCompat
+import com.andreasoftware.keuanganku.R
 import com.google.android.material.snackbar.Snackbar
 
 class MySnackbar {
@@ -8,7 +10,7 @@ class MySnackbar {
 
         fun success(view: View, message: String) {
             Snackbar.make(view, message, Snackbar.LENGTH_SHORT)
-                .setBackgroundTint(android.graphics.Color.GREEN) // Warna hijau untuk sukses
+                .setBackgroundTint(ContextCompat.getColor(view.context, R.color.green )) // Warna hijau untuk sukses
                 .setTextColor(android.graphics.Color.WHITE)
                 .show()
         }
--- app/src/main/java/com/andreasoftware/keuanganku/ui/exceptionhandler/HandleExceptionWithSnackbar.kt
+++ app/src/main/java/com/andreasoftware/keuanganku/ui/exceptionhandler/HandleExceptionWithSnackbar.kt
@@ -1,13 +1,15 @@
 package com.andreasoftware.keuanganku.ui.exceptionhandler
 
 import android.view.View
+import androidx.core.content.ContextCompat
+import com.andreasoftware.keuanganku.R
 import com.google.android.material.snackbar.Snackbar
 
 class HandleExceptionWithSnackbar {
     companion object {
         fun failed(view: View, message: String) {
             Snackbar.make(view, message, Snackbar.LENGTH_SHORT)
-                .setBackgroundTint(android.graphics.Color.RED)
+                .setBackgroundTint(ContextCompat.getColor(view.context, R.color.red ))
                 .setTextColor(android.graphics.Color.WHITE)
                 .show()
         }
