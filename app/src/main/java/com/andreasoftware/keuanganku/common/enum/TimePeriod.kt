fba162bedd3058e12a746f75bd00fd5914a1f066
andrr64 <derzandrr03@gmail.com>
2025-03-12 00:39:51 +0700

    add function to get time period and enum time period
    - Add `TimePeriod` enum for `WEEK`, `MONTH`, and `YEAR`.
    - Add `getLongTimeByPeriod` function to calculate start and end timestamps for specified `TimePeriod`, with optional `startDate` and `endDate` parameters.

---
+++ app/src/main/java/com/andreasoftware/keuanganku/common/enum/TimePeriod.kt
@@ -0,0 +1,7 @@
+package com.andreasoftware.keuanganku.common.enum
+
+enum class TimePeriod(val displayName: String) {
+    WEEK("This Week"),
+    MONTH("This Month"),
+    YEAR("This Year");
+}
---
+++ app/src/main/java/com/andreasoftware/keuanganku/common/util/GetLongTime.kt
@@ -0,0 +1,52 @@
+package com.andreasoftware.keuanganku.common.util
+
+import com.andreasoftware.keuanganku.common.enum.TimePeriod
+import java.util.*
+
+fun getLongTimeByPeriod(
+    period: TimePeriod,
+    startDate: Date? = null,
+    endDate: Date? = null
+): Pair<Long, Long> {
+    val calendar = Calendar.getInstance()
+
+    return when (period) {
+        TimePeriod.WEEK -> {
+            calendar.time = startDate ?: Date() // Gunakan startDate jika disediakan, jika tidak, gunakan waktu saat ini
+            calendar.firstDayOfWeek = Calendar.MONDAY
+            calendar.set(Calendar.DAY_OF_WEEK, calendar.firstDayOfWeek)
+            val start = calendar.timeInMillis
+
+            calendar.time = endDate ?: Date() // Gunakan endDate jika disediakan, jika tidak, gunakan waktu saat ini
+            calendar.add(Calendar.WEEK_OF_YEAR, 1)
+            calendar.add(Calendar.MILLISECOND, -1)
+            val end = calendar.timeInMillis
+
+            Pair(start, end)
+        }
+        TimePeriod.MONTH -> {
+            calendar.time = startDate ?: Date()
+            calendar.set(Calendar.DAY_OF_MONTH, 1)
+            val start = calendar.timeInMillis
+
+            calendar.time = endDate ?: Date()
+            calendar.add(Calendar.MONTH, 1)
+            calendar.add(Calendar.MILLISECOND, -1)
+            val end = calendar.timeInMillis
+
+            Pair(start, end)
+        }
+        TimePeriod.YEAR -> {
+            calendar.time = startDate ?: Date()
+            calendar.set(Calendar.DAY_OF_YEAR, 1)
+            val start = calendar.timeInMillis
+
+            calendar.time = endDate ?: Date()
+            calendar.add(Calendar.YEAR, 1)
+            calendar.add(Calendar.MILLISECOND, -1)
+            val end = calendar.timeInMillis
+
+            Pair(start, end)
+        }
+    }
+}
