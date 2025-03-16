7d5caa9d58d2e0286206b97b4fc9fc334e9f539d
andrr64 <derzandrr03@gmail.com>
2025-03-16 21:34:49 +0700

    feat: add `CurrencyFormatter` for currency formatting
    This commit introduces a new utility class, `CurrencyFormatter`, to handle currency formatting.
    - The `CurrencyFormatter` class provides a `formatCurrency` function that takes an amount (Double) and a locale as input.
    - It utilizes `NumberFormat.getCurrencyInstance` to create a currency formatter based on the specified locale.
    - It formats the given amount into a currency string according to the locale's rules.

---
+++ app/src/main/java/com/andreasoftware/keuanganku/util/CurrencyFormatter.kt
@@ -0,0 +1,11 @@
+package com.andreasoftware.keuanganku.util
+
+import java.text.NumberFormat
+import java.util.Locale
+
+object CurrencyFormatter {
+    fun formatCurrency(amount: Double, locale: Locale ): String {
+        val formatter = NumberFormat.getCurrencyInstance(locale)
+        return formatter.format(amount)
+    }
+}
