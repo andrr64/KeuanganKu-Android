4b3ef638ed574cba9d82b627d42b09007223e1b8
andrr64 <derzandrr03@gmail.com>
2025-03-24 02:03:32 +0700

    feat: add wallet form modal bottom sheet
    This commit introduces a new modal bottom sheet for adding wallet.
    
    -   Added `modal_bottomsheet_form_wallet.xml` layout for the wallet form.
    -   Created `WalletFormBSFragment` to display the bottom sheet.
    -   Created `WalletFormBSViewModel` for managing wallet data.
    -   Implemented the logic for adding new wallet to the database via `WalletFormBSFragment` and `WalletFormBSViewModel`.
    -   Added new string resources: `wallet_form`, `wallet_form_desc`, `title`, `initial_amount`, and `submit`.

---
+++ app/src/main/java/com/andreasoftware/keuanganku/ui/modal/WalletFormBSFragment.kt
@@ -0,0 +1,41 @@
+package com.andreasoftware.keuanganku.ui.modal
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.Button
+import androidx.fragment.app.viewModels
+import com.andreasoftware.keuanganku.R
+import com.andreasoftware.keuanganku.data.model.WalletModel
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import com.google.android.material.textfield.TextInputEditText
+import dagger.hilt.android.AndroidEntryPoint
+
+@AndroidEntryPoint
+class WalletFormBSFragment: BottomSheetDialogFragment() {
+
+    private val viewModel: WalletFormBSViewModel by viewModels()
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view = inflater.inflate(R.layout.modal_bottomsheet_form_wallet, container, false)
+        val submitButton = view.findViewById<Button>(R.id.submitButton)
+        val etWalletName = view.findViewById<TextInputEditText>(R.id.titleEditText)
+        val etInitialAmount = view.findViewById<TextInputEditText>(R.id.amountEditText)
+
+        submitButton.setOnClickListener {
+            val newWallet = WalletModel(name = etWalletName.text.toString(), balance = etInitialAmount.text.toString().toDouble())
+            viewModel.insertWallet(newWallet) {
+                if (it.isSuccess()) {
+                    dismiss()
+                }
+            }
+        }
+
+        return view
+    }
+}
--- app/src/main/java/com/andreasoftware/keuanganku/ui/modal/WalletFormBSViewModel.kt
+++ app/src/main/java/com/andreasoftware/keuanganku/ui/modal/WalletFormBSViewModel.kt
@@ -1,19 +1,17 @@
-package com.andreasoftware.keuanganku.ui.activity.main.page
+package com.andreasoftware.keuanganku.ui.modal
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
 import com.andreasoftware.keuanganku.common.DataOperationResult
 import com.andreasoftware.keuanganku.data.model.WalletModel
 import com.andreasoftware.keuanganku.data.repository.WalletRepository
-import com.andreasoftware.keuanganku.data.repository.cache.IncomeCacheRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
 import kotlinx.coroutines.launch
 import javax.inject.Inject
 
 @HiltViewModel
-class PageWalletFormViewModel
-@Inject constructor(val repository: WalletRepository, val incomeCache: IncomeCacheRepository) :
-    ViewModel() {
+class WalletFormBSViewModel
+@Inject constructor(val repository: WalletRepository) : ViewModel() {
     fun insertWallet(wallet: WalletModel, callback: (DataOperationResult) -> Unit) {
         viewModelScope.launch {
             val result = repository.insertWallet(wallet)
