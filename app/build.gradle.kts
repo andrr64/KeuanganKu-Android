9d92ecad311db8afa3c78ed757e9190642b9caca
andrr64 <derzandrr03@gmail.com>
2025-03-24 21:01:27 +0700

    Update `minSdk` to 26 and `versionName` to 1.0.0
    - Changed `minSdk` from 24 to 26.
    - Updated `versionName` from "1.0" to "1.0.0".

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,10 +12,10 @@ android {
 
     defaultConfig {
         applicationId = "com.andreasoftware.keuanganku"
-        minSdk = 24
+        minSdk = 26
         targetSdk = 35
         versionCode = 1
-        versionName = "1.0"
+        versionName = "1.0.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
